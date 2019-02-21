152ffdd49c4e93fc771796faa4289596d3549e7d
Roasallam <ruaa.sallam1997@hotmail.com>
2019-02-21 21:06:21 +0200

    implementation improvement in snack machine class

--- src/main/java/com/progressoft/induction/SnackMachine.java
+++ src/main/java/com/progressoft/induction/SnackMachine.java
@@ -6,15 +6,25 @@ import java.util.HashSet;
 public class SnackMachine implements Machine {
 
    static int DEFAULT_QUANTITY = 10;
-   static Money MONEY_INSIDE = Money.ZERO;
-   static Money MONEY_IN_TRANSACTION = Money.ZERO;
-   static Money CHANGE = Money.ZERO;
-   static HashSet<Double> acceptedMoney = setAcceptedMoney();
-   static Snack chewingGums = new Snack(DEFAULT_QUANTITY, SnackType.CHEWING_GUM, BigDecimal.valueOf(0.5));
-   static Snack chips = new Snack(DEFAULT_QUANTITY, SnackType.CHIPS, BigDecimal.valueOf(1.0));
-   static Snack chocolate = new Snack(DEFAULT_QUANTITY, SnackType.CHOCOLATE, BigDecimal.valueOf(2.0));
+   Money moneyInside;
+   Money moneyInTransaction;
+   Money change;
+   HashSet<Double> acceptedMoney;
+   Snack chewingGums;
+   Snack chips;
+   Snack chocolate;
 
-   private static HashSet<Double> setAcceptedMoney () {
+   public SnackMachine() {
+      moneyInside = new Money(BigDecimal.valueOf(0));
+      moneyInTransaction = new Money(BigDecimal.valueOf(0));
+      change = new Money(BigDecimal.valueOf(0));
+      acceptedMoney = setAcceptedMoney();
+      chewingGums = new Snack(DEFAULT_QUANTITY, SnackType.CHEWING_GUM, BigDecimal.valueOf(0.5));
+      chips = new Snack(DEFAULT_QUANTITY, SnackType.CHIPS, BigDecimal.valueOf(1.0));
+      chocolate = new Snack(DEFAULT_QUANTITY, SnackType.CHOCOLATE, BigDecimal.valueOf(2.0));
+   }
+
+   private HashSet<Double> setAcceptedMoney () {
       HashSet<Double> acceptedMoney = new HashSet<>();
 
       for (MoneyUnit moneyUnit : MoneyUnit.values()) {
@@ -24,18 +34,32 @@ public class SnackMachine implements Machine {
    }
 
    public Money moneyInside () {
-      return MONEY_INSIDE;
+      return moneyInside;
+   }
+
+   public Money moneyInTransaction () {
+      return moneyInTransaction;
+   }
+
+   public Money change () { return change; }
+
+   Snack chewingGums () {
+      return chewingGums;
+   }
+
+   Snack chips () {
+      return chips;
+   }
+
+   Snack chocolates () {
+      return chocolate;
    }
 
    public void insertMoney (Money money) {
       if ((money == null) || !(isAcceptedMoney(money)))
          throw new IllegalArgumentException("Unsupported money unit");
 
-      MONEY_IN_TRANSACTION = MONEY_IN_TRANSACTION.add(money);
-   }
-
-   public Money moneyInTransaction () {
-      return MONEY_IN_TRANSACTION;
+      moneyInTransaction = moneyInTransaction.add(money);
    }
 
    public Money buySnack (SnackType snackType) {
@@ -55,26 +79,14 @@ public class SnackMachine implements Machine {
       if(check)
          buy(snack);
 
-      return CHANGE;
-   }
-
-   Snack chewingGums () {
-      return chewingGums;
-   }
-
-   Snack chips () {
-      return chips;
-   }
-
-   Snack chocolates () {
-      return chocolate;
+      return change;
    }
 
    void buy (Snack snack) {
-      BigDecimal newValue = MONEY_IN_TRANSACTION.getValue();
-      MONEY_INSIDE.setValue(newValue);
+      BigDecimal newValue = moneyInTransaction.getValue();
+      moneyInside.setValue(newValue);
       change(snack);
-      MONEY_IN_TRANSACTION = new Money(BigDecimal.valueOf(0));
+      moneyInTransaction = new Money(BigDecimal.valueOf(0));
       //MONEY_IN_TRANSACTION.setValue(BigDecimal.valueOf(0));
       snack.decreaseQuantity();
    }
@@ -82,8 +94,8 @@ public class SnackMachine implements Machine {
    void change (Snack snack) {
       Money price = new Money(snack.getPrice());
 
-      if (price.isLessThan(MONEY_INSIDE))
-         CHANGE.setValue(MONEY_INSIDE.subtract(price).getValue());
+      if (price.isLessThan(moneyInside))
+         change.setValue(moneyInside.subtract(price).getValue());
    }
 
    boolean check (Snack snack) {
@@ -105,7 +117,7 @@ public class SnackMachine implements Machine {
       BigDecimal price = snack.getPrice();
       Money money = new Money(price);
 
-      return money.isLessThan(MONEY_IN_TRANSACTION) || money.isEqual(MONEY_IN_TRANSACTION);
+      return money.isLessThan(moneyInTransaction) || money.isEqual(moneyInTransaction);
    }
 
    boolean isAvailable (Snack snack) {
