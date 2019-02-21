d2421f07968b33220b0f5ad63f0d3ac57784e2e1
Roasallam <ruaa.sallam1997@hotmail.com>
2019-02-21 17:50:43 +0200

    set up money class

---
+++ src/main/java/com/progressoft/induction/Money.java
@@ -0,0 +1,71 @@
+package com.progressoft.induction;
+
+import java.math.BigDecimal;
+
+public class Money {
+
+   static Money ZERO = new Money(BigDecimal.valueOf(0));
+   static Money QUARTER_DINAR = new Money(BigDecimal.valueOf(0.25));
+   static Money HALF_DINAR = new Money(BigDecimal.valueOf(0.5));
+   static Money DINAR = new Money(BigDecimal.valueOf(1));
+
+
+   BigDecimal value;
+
+   Money(BigDecimal value) {
+      if (value == null)
+         throw new IllegalArgumentException("value can't be null");
+
+      if (value.compareTo(BigDecimal.ZERO) < 0)
+         throw new IllegalArgumentException("value can't be negative");
+
+      this.value = value;
+   }
+
+   BigDecimal getValue() {
+      return value;
+   }
+
+   public void setValue(BigDecimal value) {
+      this.value = value;
+   }
+
+   Money add (Money money) {
+      if (money == null)
+         return this;
+
+      BigDecimal value = this.getValue().add(money.getValue());
+
+      return new Money(value);
+   }
+
+   Money subtract (Money money) {
+      if (money == null)
+         return this;
+
+      if (this.isLessThan(money))
+         throw new IllegalArgumentException("can't be subtracted");
+
+      BigDecimal value = this.getValue().subtract(money.getValue());
+
+      return new Money(value);
+   }
+
+   boolean isLessThan (Money money) {
+      if (money == null)
+         return false;
+
+      BigDecimal value = money.getValue();
+
+      return this.getValue().compareTo(value) < 0;
+   }
+
+   boolean isEqual (Money money) {
+      if (money == null)
+         return false;
+
+      BigDecimal value = money.getValue();
+
+      return this.getValue().compareTo(value) == 0;
+   }
+}
